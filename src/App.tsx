fc7002e8b738162a444bd547de2d83ed7849c873
Lauro Almeida <lauro.almeida@uniriotec.br>
2022-10-09 09:51:35 -0300

    Implementing routes, layouts and home component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import { ThemeProvider } from 'styled-components'
-import { Button } from './components/Button'
+import { BrowserRouter } from 'react-router-dom'
 
+import { Router } from './Router'
+
+import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary" />
-      <Button variant="secondary" />
-      <Button variant="danger" />
-      <Button variant="success" />
-
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
